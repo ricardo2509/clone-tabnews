eb38f59bb54c661c4001bccd62073abd5ba803fb
ricardo2509 <ricardo2509@gmail.com>
2025-12-05 15:31:15 +0000

    add logo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home(){
-    return <h1>RMS Monitoring Health</h1>
+    return 
+        <html>
+            <h1>RMS Monitoring Health</h1>
+            <br />
+            <img src="/logo_circle.png" alt="RMS Health Status"/>
+        </html>
 }
 
 export default Home;
