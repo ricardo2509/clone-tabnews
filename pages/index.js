d1bcce4e00ed7c2dcd706f906847b7b86b37bb90
ricardo2509 <ricardo2509@gmail.com>
2025-12-05 15:49:44 +0000

    remove  logo

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 function Home(){
-    return 
-        <html>
-            <h1>RMS Monitoring Health</h1>
-            <br />
-            <img src="/logo_circle.png" alt="RMS Health Status"/>
-        </html>
+    return <h1>RMS Monitoring</h1>
 }
 
 export default Home;
