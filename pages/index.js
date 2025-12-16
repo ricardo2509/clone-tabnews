d8d3b9d32135a8601a4871681c24f205d9c337e5
ricardo2509 <ricardo2509@gmail.com>
2025-12-16 10:09:27 -0300

    Alterado texto página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import foto from '../public/images/logo_circle.png';
 
 function Home() {
-    return <div>
-        <img src={`/images/logo_small.png`} alt="image not found" />
+    return <div align="center">
+        <img src={`/images/logo_small.png`} width="60" alt="image not found" />
         <br />
         <h1>RMS Monitoring</h1><h2>We at RMS Monitoring are looking for app developers</h2>
         <br />
-        <h2>Our focus is on monitoring post-COVID patients, with an emphasis on tracking clinical signs to help us direct physioterapy professionals for home care</h2>
+        <h2>Our focus is on monitoring post-COVID patients, with an emphasis on tracking</h2>
+        <h2>clinical signs to help us direct physioterapy professionals for home care</h2>
     </div>;
 }
 
