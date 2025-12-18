9fae6a5c7fe8553d1f7b2eec526a77cef07e0c36
ricardo2509 <ricardo2509@gmail.com>
2025-12-18 11:48:20 +0000

    Fix image name (case) and excluding extra images

--- components/inProgress.js
+++ components/inProgress.js
@@ -1,12 +1,12 @@
 import s from "/pages/styles.module.css";
 
-function InProgress() { 
+function InProgress() {
     return <div className={s.pageContainer}>
         <div className={s.mainContent}>
-            <img className={s.centerDiv} src={`/images/WiP00.png`} width="300px" alt="image not found" />
+            <img className={s.centerDiv} src={`/images/Wip00.png`} width="300px" alt="image not found" />
             <h2 className={`${s.hMessage} ${s.hWarningMessage} ${s.centerDiv}`}>This page is under construction!</h2>
-       </div>
-       </div>
+        </div>
+    </div>
 }
 
 export default InProgress;
